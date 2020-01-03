d365d4f67e83e2fa04518cb8094737afe315a4d7
Adam Xiao <adam.x.xiao@gmail.com>
2020-01-03 03:32:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -13,9 +13,10 @@ def reverse_each_word(sentence)
 #      puts reversed_words_string
 #    end
 
-  sentence
-  reverse_each_word.each do |result|
-      puts "#{result.reverse.split.reverse.join(" ")}"
-  end
+sentence
 
+end
+
+reverse_each_word.each do |result|
+    puts "#{result.reverse.split.reverse.join(" ")}"
 end
