875d365ad9616c435b7e3db7c81b633d635d6bb2
Adam Xiao <adam.x.xiao@gmail.com>
2020-01-03 02:30:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(sentence)
   x = 0
 
   sentence_array.each do |reveresal|
-    reversed_words_array = reversal[x].reverse
+    reversed_words_array << reversal[x].reverse
     x += 1
     end
   end
