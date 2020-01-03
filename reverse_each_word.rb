3fabba0d0c52de069cf45ab9fa0ee073ee98c301
Adam Xiao <adam.x.xiao@gmail.com>
2020-01-03 02:59:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,16 @@
 def reverse_each_word(sentence)
-  sentence_array = sentence.split(" ")
-  reversed_words_array = []
-  reversed_words_string = ""
-    sentence_array.each do |reversal|
-    x = 0
-
-      while x < sentence_array.length do
-        reversed_words_array << "#{reversal[x].reverse}"
-        x += 1
-      end
-      reversed_words_string = reversed_words_array.join(" ")
-      puts reversed_words_string
-    end
-
+#  sentence_array = sentence.split(" ")
+#  reversed_words_array = []
+#  reversed_words_string = ""
+#    sentence_array.each do |reversal|
+#    x = 0
+#
+#      while x < sentence_array.length do
+#        reversed_words_array << "#{reversal[x].reverse}"
+#        x += 1
+#      end
+#      reversed_words_string = reversed_words_array.join(" ")
+#      puts reversed_words_string
+#    end
+  sentence.reverse
 end
