1f26ced76263a83aee24868cc12f2ee1f4aa99a4
Adam Xiao <adam.x.xiao@gmail.com>
2020-01-03 04:17:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,8 +4,6 @@ def reverse_each_word(sentence)
 #new_sentence = sentence.reverse.split.reverse.join(" ")
 
 sentence_array = sentence.split
-sentence_array.each do |x|
-  x.reverse
-  puts x
+sentence_array.each { |word| word.reverse }
 end
 end
